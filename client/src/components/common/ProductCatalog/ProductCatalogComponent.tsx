0200b5b46cd0bf366845bb6b6559c2f13b779cd6
Chien <cn555444@gmail.com>
2023-05-17 19:32:44 +0700

    update product catalog

--- client/src/components/common/ProductCatalog/ProductCatalogComponent.tsx
+++ client/src/components/common/ProductCatalog/ProductCatalogComponent.tsx
@@ -29,8 +29,8 @@ export const ProductsColumn = (props: IProductLayout) => {
     <Grid key={props.product.id} item xs={props.cols} sx={productStyles}>
       <Box sx={imgProductStyles}>
         <img
-          style={{ width: "100%" }}
-          src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
+          style={{ width: "100%", objectFit: "cover" }}
+          src={props.product.image}
           alt=""
         />
         <div className="hover">
