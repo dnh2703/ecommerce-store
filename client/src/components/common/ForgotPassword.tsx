756c82e08611e2858fb58546b9c301b66810a5a7
duongphong12 <duongphongnt@gmail.com>
2023-05-27 11:37:27 +0700

    update forgotpasswor and signin

---
+++ client/src/components/common/ForgotPassword.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import authApi from "../../api/authApi";
+import { IForgotPassword } from "../../interfaces/auth";
+import { toast } from "react-toastify";
+export default function ForgotPassword(props: any) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IForgotPassword>();
+
+  const { setForgotPassword } = props;
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [errmessage, setErrmessage] = useState("");
+  const [succesmessage, setSuccesmessage] = useState("");
+
+  const handleSubmitForgotPassword = (data: IForgotPassword) => {
+    setLoading(true);
+    console.log(data);
+    authApi
+      .forgotPassword(data)
+      .then((response) => {
+        if (response.status === 200) {
+          alert("We've sent you an email with a link to update your password.");
+          setSuccesmessage(
+            "We've sent you an email with a link to update your password."
+          );
+        }
+
+        setForgotPassword();
+      })
+
+      .catch((err) => {
+        setErrmessage("No account found with that email.");
+      });
+  };
+  const handleSetForgotPassword = () => {
+    setForgotPassword();
+  };
+  return (
+    <div className="w-2/4 box-border max-[700px]:w-full">
+      <div>
+        <h1 className="text-4xl">Reset your password</h1>
+        <p className="my-6 text-xl text-gray-500">
+          We will send you an email to reset your password.
+        </p>
+      </div>
+      <form onSubmit={handleSubmit(handleSubmitForgotPassword)}>
+        <div className="text-red-600 ">
+          {errmessage && <p className="">{errmessage}</p>}
+          <div className="text-green-600 ">
+            {succesmessage && <p className="">{succesmessage}</p>}
+          </div>
+        </div>
+
+        <input
+          className="border border-slate-400 h-12 w-full mt-4 max-[700px]:w-full pl-2 mb-2 "
+          id="email"
+          placeholder="Email"
+          {...register("email", {
+            required: true,
+
+            pattern: /^\S+@\S+$/i,
+          })}
+        />
+        {errors.email?.type === "required" && (
+          <p className="text-red-600">you can not your email</p>
+        )}
+        {errors.email?.type === "pattern" && (
+          <p className="  text-red-600">Please enter a valid email address</p>
+        )}
+        <div className="mt-6">
+          <button className=" text-xl  border border-slate-400 h-14 w-1/2 hover:bg-red-900 bg-slate-900 text-white  max-[700px]:w-full">
+            {loading ? "loading..." : "SUBMIT"}
+          </button>
+          <p
+            onClick={handleSetForgotPassword}
+            className="max-[700px]:w-full max-[700px]: mt-4  text-xl w-2/4 inline-block text-center cursor-pointer underline"
+          >
+            Cancel
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+}
